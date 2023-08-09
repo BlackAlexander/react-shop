cc3d4ba500286775330cb9589385ee5b1914e11e
bitstone-alexandru-negru <acon3331@yahoo.com>
2023-08-09 17:22:47 +0300

    Bestoptimized fetches in the whole internship team

--- shop/src/CartPage/Cart.js
+++ shop/src/CartPage/Cart.js
@@ -6,16 +6,14 @@ import CartFooter from "../CartPage/CartFooter";
 import {useState} from "react";
 
 export default function Cart(){
-    const [key, setKey] = useState(0);
     const [cartItems, setCartItems] = useState([])
     const updateItemsData = (newList) => {
         setCartItems(newList);
-        setKey(key + 1);
     }
     return (
         <>
             <CartHeader />
-            <CartDisplay updateItemsData={updateItemsData} key={key}/>
+            <CartDisplay updateItemsData={updateItemsData}/>
             <CartSummary />
             <CartFooter />
         </>
