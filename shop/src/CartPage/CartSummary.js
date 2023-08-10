728cd5b214d8703c334db0be33b789274f80ceee
bitstone-alexandru-negru <acon3331@yahoo.com>
2023-08-10 11:04:43 +0300

    Add animations

--- shop/src/CartPage/CartSummary.js
+++ shop/src/CartPage/CartSummary.js
@@ -3,13 +3,13 @@ import {undone} from "../CartPage/CartAuxJS";
 export default function CartSummary(){
     return <div className="cart-summary">
         <div className="summary-total">TOTAL</div>
-        <div className="summary-total-value">##VALUE##</div>
+        <div className="summary-total-value">0</div>
         <div className="summary-items">Items</div>
-        <div className="summary-items-value">##VALUE##</div>
+        <div className="summary-items-value">0</div>
         <div className="summary-quantity">Quantity</div>
-        <div className="summary-quantity-value">##VALUE##</div>
+        <div className="summary-quantity-value">0</div>
         <div className="summary-price">PRICE</div>
-        <div className="summary-price-value">##VALUE##</div>
+        <div className="summary-price-value">0</div>
         <div className="summary-order" onClick={undone}>Proceed to payment</div>
     </div>
 }
