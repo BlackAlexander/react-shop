b25a8013d7f0ac75d888996d3ee9503f60134d08
bitstone-alexandru-negru <acon3331@yahoo.com>
2023-08-10 17:12:52 +0300

    Clean redux to try again

--- shop/src/CartPage/Cart.js
+++ shop/src/CartPage/Cart.js
@@ -4,29 +4,21 @@ import CartDisplay from "../CartPage/CartDisplay";
 import CartSummary from "../CartPage/CartSummary";
 import CartFooter from "../CartPage/CartFooter";
 import {useState} from "react";
-import {Provider, useDispatch} from "react-redux";
-import store from '../redux/store.js'
+import {Provider} from "react-redux";
 
 export default function Cart(){
     const [cartItems, setCartItems] = useState([])
     const updateItemsData = (newList) => {
         setCartItems(newList);
     }
-    const dispatch = useDispatch();
 
-    const updateCartItems = (newList) => {
-        // Dispatch the action with updated cartItems
-        dispatch({ type: 'UPDATE_CART_ITEMS', payload: newList });
-    }
 
     return (
         <>
-            <Provider store={store}>
-                <CartHeader />
-                <CartDisplay updateItemsData={updateItemsData}/>
-                <CartSummary />
-                <CartFooter />
-            </Provider>
+            <CartHeader />
+            <CartDisplay updateItemsData={updateItemsData}/>
+            <CartSummary />
+            <CartFooter />
         </>
     )
 }
--- shop/src/CartPage/CartDisplay.js
+++ shop/src/CartPage/CartDisplay.js
@@ -1,10 +1,9 @@
 import {returnCartItems} from "../CartPage/CartAuxJS";
-import {useSelector} from "react-redux";
 
 export default function CartDisplay({updateItemsData}){
-    // const itemsList = returnCartItems("http://vlad-matei.thrive-dev.bitstoneint.com/wp-json/internship-api/v1/cart/", updateItemsData);
     const itemsList = returnCartItems("http://vlad-matei.thrive-dev.bitstoneint.com/wp-json/internship-api/v1/cart/", updateItemsData);
-    const cartItems = useSelector((state) => state.cart.cartItems);
+
+
     return <div className="cart-display">
         <h2 className="cart-title">YOUR CART</h2>
         <div className="cart-items" id="cart-items-list">
