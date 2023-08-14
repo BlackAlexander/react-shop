b11cc3bd49eeee8df4b7db5fc5f218758983ef9a
bitstone-alexandru-negru <acon3331@yahoo.com>
2023-08-15 00:24:23 +0300

    Display reviews in product page

--- shop/src/PreviewPage/Preview.js
+++ shop/src/PreviewPage/Preview.js
@@ -4,12 +4,42 @@ import {useEffect, useState} from "react";
 import {useGetProductQuery} from "../redux/apis.js";
 import {getToken} from "../DisplayPage/DisplayAuxJS.js";
 import {useLocation} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {selectRatings} from "../redux/slices/ratings";
+
+
+function RatingPreview({rpId, rpTitle, rpDescription, rpRating}){
+    let newId = "review-" + String(rpId);
+    return <div id={newId} key={newId} className="rp-container">
+        <div className="rp-title">{rpTitle}</div>
+        <div className="rp-description">{rpDescription}</div>
+        <div className="rp-rating">{rpRating}⭐</div>
+    </div>
+}
 
 export default function Preview(){
-    /** @namespace currentItem.rating **/
     /** @namespace product.images **/
     let location = useLocation();
     const id = location.pathname.slice(6);
+
+    const listOfRatings = useSelector(selectRatings);
+    let theseRatings = [];
+    for (let i = 0; i < listOfRatings.length; i++){
+        const thisId = listOfRatings[i].id;
+        if (thisId === id){
+            theseRatings.push(listOfRatings[i]);
+        }
+    }
+    let ratings = [];
+    for (let i = 0; i < theseRatings.length; i++){
+        let therating = theseRatings[i];
+        ratings.push(RatingPreview({
+            rpId: String(i),
+            rpDescription: therating.description,
+            rpRating: therating.rating,
+            rpTitle: therating.title
+        }))
+    }
 
     const [currentItem, setCurrentItem] = useState(undefined);
     const [currentImage, setCurrentImage] = useState("https://picsum.photos/200/300");
@@ -29,7 +59,7 @@ export default function Preview(){
     }
 
     const images = currentItem.images;
-    const rating = currentItem.rating;
+    const starrating = currentItem.rating;
 
     function leftPic(){
         for (let i = 1; i < images.length; i++){
@@ -62,6 +92,7 @@ export default function Preview(){
                 <img className="preview-image" src={currentImage} alt="Product Preview" />
                     <div className="preview-title">{currentItem.title}</div>
                     <div className="preview-description">{currentItem.description}</div>
+                    <div className="preview-ratings-holder">{ratings}</div>
                     <div className="preview-price">${currentItem.price}</div>
                     {/*<div className="preview-add-to-cart" id="previewbtn">🛒 ADD</div>*/}
                     <div className="preview-rating">
