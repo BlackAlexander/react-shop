68c496c5071b8846eb8f7582410057d22bfd042e
bitstone-alexandru-negru <acon3331@yahoo.com>
2023-08-25 15:41:36 +0300

    Finish all order features

--- shop/src/OrdersPage/OrderPreview.js
+++ shop/src/OrdersPage/OrderPreview.js
@@ -1,39 +1,40 @@
+import OrderProduct from "./OrderProduct";
+
 export default function OrderPreview({number, date, status, address, payment, products}){
-    console.log(number);
+    let orderno = "ORDER no. ";
+    orderno += String(number);
+    let statusnew = "status: ";
+    statusnew += status;
+    let priceSum = 0;
+    for (let i in products){
+        priceSum += products[i].price;
+    }
+    const totalno = "Total: $" + String(priceSum);
+
+    let listOfProducts = [];
+    for (let i in products){
+        const currentItem = products[i];
+        listOfProducts.push(OrderProduct({
+            itemId: currentItem.id,
+            itemPic: currentItem.thumbnail,
+            itemPrice: currentItem.price,
+            itemQuantity: currentItem.quantity,
+            itemTitle: currentItem.title,
+            forkey: i
+        }))
+    }
+
     return (
         <>
             <div className="order-preview-container">
                 <div className="order-header">
-                    <div className="order-preview-number">ORDER no. 136752</div>
-                    <div className="order-preview-status">status: paid</div>
-                    <div className="order-preview-date">23.10.2023</div>
-                    <div className="order-preview-address">Calea Turzii 36</div>
+                    <div className="order-preview-number">{orderno}</div>
+                    <div className="order-preview-status">{statusnew}</div>
+                    <div className="order-preview-date">{date}</div>
+                    <div className="order-preview-address">{address}</div>
                 </div>
                 <div className="order-products">
-                    <div className="order-product" id="order-product-1">
-                        <img src="https://picsum.photos/80/100" alt="order product" className="order-product-pic"/>
-                        <div className="order-product-title">Burj Khalifa</div>
-                        <div className="order-product-price">$1092.50</div>
-                        <div className="order-product-quantity">x230</div>
-                    </div>
-                    <div className="order-product" id="order-product-2">
-                        <img src="https://picsum.photos/80/101" alt="order product" className="order-product-pic"/>
-                        <div className="order-product-title">Mia Tyler</div>
-                        <div className="order-product-price">$593.4</div>
-                        <div className="order-product-quantity">x1</div>
-                    </div>
-                    <div className="order-product" id="order-product-3">
-                        <img src="https://picsum.photos/89/100" alt="order product" className="order-product-pic"/>
-                        <div className="order-product-title">Bob</div>
-                        <div className="order-product-price">$293.4</div>
-                        <div className="order-product-quantity">x4</div>
-                    </div>
-                    <div className="order-product" id="order-product-4">
-                        <img src="https://picsum.photos/81/100" alt="order product" className="order-product-pic"/>
-                        <div className="order-product-title">Love</div>
-                        <div className="order-product-price">$0</div>
-                        <div className="order-product-quantity">x0</div>
-                    </div>
+                    {listOfProducts}
                 </div>
                 <div className="order-footer">
                     <div className="order-preview-payment">Card Payment</div>
@@ -43,7 +44,7 @@ export default function OrderPreview({number, date, status, address, payment, pr
                         <div className="order-preview-cardexpire">3/2028</div>
                         <div className="order-preview-cardcvv">461</div>
                     </div>
-                    <div className="order-preview-total">Total: $18930.25</div>
+                    <div className="order-preview-total">{totalno}</div>
                 </div>
             </div>
         </>
--- shop/src/OrdersPage/OrderProduct.js
+++ shop/src/OrdersPage/OrderProduct.js
@@ -1,8 +1,8 @@
-export default function PlaceProduct({itemId, itemPic, itemTitle, itemQuantity, itemPrice}) {
-    const newId = "order-product-" + String(itemId);
+export default function OrderProduct({itemPic, itemTitle, itemQuantity, itemPrice, forkey}){
     const newPrice = "$" + String(itemPrice);
     const newQuantity = "x" + String(itemQuantity);
-    return <div className="order-product" id={newId} key={newId}>
+    const newKey = "order-product-" + String(forkey);
+    return <div className="order-product" id={newKey} key={newKey}>
         <img src={itemPic} alt="order product" className="order-product-pic"/>
         <div className="order-product-title">{itemTitle}</div>
         <div className="order-product-price">{newPrice}</div>
